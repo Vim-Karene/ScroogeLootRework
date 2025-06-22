255d8ffac199399aac3f996145a8d6f2517a590c
Kärene <143636066+Vim-Karene@users.noreply.github.com>
2025-06-22 16:46:09 +0200

    Add compatibility for UnitIsGroupLeader

--- compat.lua
+++ compat.lua
@@ -16,3 +16,31 @@ function addon:GetNumGroupMembers()
         return GetNumPartyMembers()
     end
 end
+
+function addon:UnitIsGroupLeader(unit)
+    if UnitIsGroupLeader then
+        return UnitIsGroupLeader(unit)
+    end
+    unit = unit or "player"
+    if unit == "player" then
+        if addon:IsInRaid() then
+            return IsRaidLeader()
+        elseif addon:IsInGroup() then
+            return IsPartyLeader()
+        end
+        return true
+    end
+
+    if UnitInRaid(unit) then
+        local index = GetPartyLeaderIndex()
+        return index > 0 and UnitIsUnit(unit, "raid" .. index)
+    elseif UnitInParty(unit) then
+        local index = GetPartyLeaderIndex()
+        if index == 0 then
+            return unit == "player"
+        else
+            return UnitIsUnit(unit, "party" .. index)
+        end
+    end
+    return false
+end
